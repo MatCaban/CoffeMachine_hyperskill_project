2bf70a8c40346ac2a91ffcd81e484540add44e4c
Matus Caban <caban.matus@gmail.com>
2025-05-05 20:15:49 +0200

    Add Ingredients class, that should manage ingredients

---
+++ Ingredients.java
@@ -0,0 +1,41 @@
+package machine;
+
+public class Ingredients {
+    private int water;
+    private int milk;
+    private int coffeeBeans;
+
+    public int getCoffeeBeans() {
+        return coffeeBeans;
+    }
+
+    public void setCoffeeBeans(int coffeeBeans) {
+        this.coffeeBeans = coffeeBeans;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+
+    public Ingredients(int water, int milk, int coffeeBeans) {
+        this.water = water;
+        this.milk = milk;
+        this.coffeeBeans = coffeeBeans;
+    }
+
+    public int getWater() {
+        return this.water;
+    }
+
+    public void setWater(int water) {
+        this.water += water;
+    }
+
+
+
+
+}
